cb8c73f10754afa0879571e2ac6a20a8e7536915
infan11 <infanjiounrahman20606@gmail.com>
2024-02-19 08:19:09 +0600

    added Main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../componenets/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            
         </div>
     );
 };
