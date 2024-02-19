95b68b402e5fd7eaebf48cafbe383506305fb0ab
infan11 <infanjiounrahman20606@gmail.com>
2024-02-19 08:15:12 +0600

    added Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,43 @@
 
 import {createBrowserRouter} from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../componenets/Home/Home/Home";
+import AddBooks from "../componenets/AddBooks/AddBooks/AddBooks";
+import AlllBoooks from "../componenets/AlllBooks/AllBooks/AlllBoooks";
+import BorrowedBooks from "../componenets/BorrowdBooks/BorrowedBooks/BorrowedBooks";
+import Login from "../componenets/Verify/Login/Login";
+import Register from "../componenets/Verify/Register/Register";
 
 export   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
+      // TODO : Error Element
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path: "/addBooks",
+            element: <AddBooks></AddBooks>
+        },
+        {
+            path: "/allBooks",
+            element:<AlllBoooks></AlllBoooks>
+        },
+        {
+            path: "/borrowedBooks",
+            element: <BorrowedBooks></BorrowedBooks>
+        },
+        {
+            path: "/login",
+            element : <Login></Login>
+        },
+        {
+            path: "/register",
+            element: <Register></Register>
+        }
+      ]
     },
   ]);
