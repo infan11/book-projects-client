f0d6ec8f6e69b9dae19b9c2ab92778435c384fdb
infan11 <infanjiounrahman20606@gmail.com>
2024-02-20 10:45:04 +0600

    firebase and .env.local register

--- src/componenets/Verify/Register/Register.jsx
+++ src/componenets/Verify/Register/Register.jsx
@@ -2,16 +2,19 @@
  import { FcGoogle } from "react-icons/fc";
  import { FaGithub } from "react-icons/fa";
 import './regsiter.css'
+import { Link } from 'react-router-dom';
 const Register = () => {
     return (
         <div>
          <div className="hero min-h-screen max-w-full mx-auto">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center hidden lg:block lg:text-left ">
-       <img src={image} className=' ml-14 w-[700px]   ' alt="" />
+       <img src={image} className=' imageR ml-14 w-[700px]   ' alt="" />
     </div>
     <div className=" fromDiv rounded-lg  md:w-96 md:px-8  lg:w-ful  bg-white ">
+    <p className='text-[15px] text-center' > You Have An Account ? Now <Link className='text-blue-400 font-bold' to={"/login"}>SignIn</Link></p>
       <form className=" lg:w-1/2  lg:mx-auto lg:ml-2  "> 
+    
         <div className="form-control">
           <label className="label">
             <span className="label-textfont-bold font-mono  text-black">Name</span>   
@@ -39,13 +42,13 @@ const Register = () => {
 
         <div className="form-control mt-6 ">
         <button className="formInput   p-2 bg-blue-400 md:w-60 rounded-lg text-white font-bold  lg:w-80">Continue</button>
-        <div className='divider  '>or</div>
+        <div className='divider  lg:ml-[141px]'>or</div>
         <div className='formInput   p-2 bg-white md:w-60 rounded-lg   font-bold  lg:w-80'>
-        <button className="flex items-center gap-2 ml-2 text-black"><FcGoogle /> Continue With Google</button>
+        <button className="flex items-center gap-2  md:ml-5 lg:ml-12 text-black"><FcGoogle /> Continue With Google</button>
         </div>
         
-        <div className='formInput   p-2 bg-white md:w-60 rounded-lg   font-bold  lg:w-80'>
-        <button className="flex items-center gap-2 ml-2  text-black"><FaGithub /> Continue With Github</button>
+        <div className='formInput mt-2  p-2 bg-white md:w-60 rounded-lg   font-bold  lg:w-80'>
+        <button className="flex items-center gap-2 md:ml-5 lg:ml-12    text-black"><FaGithub /> Continue With Github</button>
         </div>
        
         
