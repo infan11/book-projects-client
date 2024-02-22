323a5d121fdea13d92e42838e3e40791467b64dc
infan11 <infanjiounrahman20606@gmail.com>
2024-02-22 15:51:57 +0600

    popular card

--- src/componenets/Home/Populer/Popular.jsx
+++ src/componenets/Home/Populer/Popular.jsx
@@ -13,10 +13,10 @@ const Popular = () => {
              <h2 className=" lg:ml-9 mt-4 text-3xl font-bold text-h font-mono text-black">Popular Books in BookShop</h2>
              <div className='grid md:grid-cols-2  mb-5'>
                 <p className='mb-2 lg:ml-9 text-black font-bold'>Journey to Faith" explores the profound teachings of Islam, offering spiritual insights, fostering peace, and guiding readers towards enlightenment and virtue.</p>
-                <button className='  bg-blue-600 text-white p-2 w-44 font-bold text-xl  rounded lg:ml-[420px]'>Browse all Books</button>
+                <button className='bg-blue-600 text-white p-2 w-44 font-bold text-xl  rounded-xl lg:ml-[420px]'>Browse all Books</button>
              </div>
             </div>
-            <div  className=' max-w-7xl  grid md:grid-cols-4 '>
+            <div  className=' md:max-w-7xl  grid md:grid-cols-4 md:ml-10 '>
            {
              books.map(book => <PopularCard key={book.id} book={book}></PopularCard> )
            }
