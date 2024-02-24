6a7310ad91898dd6f0e89c2c560661cec7467952
infan11 <infanjiounrahman20606@gmail.com>
2024-02-24 08:02:19 +0600

    add allcard jsx

--- src/componenets/AlllBooks/AllBooksCard/AllBooksCard.jsx
+++ src/componenets/AlllBooks/AllBooksCard/AllBooksCard.jsx
@@ -1,23 +1,21 @@
 
 
 const AllBooksCard = ({books}) => {
-    const {_id , email , image , name , quantity , author , shortdescription ,  rating ,  price} = books;
+    const {_id , image , name , author ,   rating , category } = books;
      
     return (
         <div>
-            <div className="card w-80 shadow-xl">
+            <div className=" w-64 h-[550px]  block shadow-xl">
   <figure className="px-2 pt-2">
-    <img src={image} alt="Shoes" className="rounded-xl" />
+    <img src={image} alt="Shoes" className="rounded-xl w-64 h-72 " />
   </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{name}</h2>
-    <h2 className="card-title">{author}</h2>
-    <h2 className="card-title">{rating}</h2>
-    <h2 className="card-title">{price}</h2>
-    
-    <p>{shortdescription}</p>
+  <div className="card-body">
+    <h2 className="text-[17px]  font-black text-red-700" >{name}</h2>
+    <h2 className="text-[15px]  font-black text-yellow-500">{author}</h2>
+    <h2 className="text-[14px]  font-black text-black">{rating}</h2>
+    <h2 className="text-[14px]  font-black text-orange-400">{category}</h2>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-block">Details</button>
     </div>
   </div>
 </div>
